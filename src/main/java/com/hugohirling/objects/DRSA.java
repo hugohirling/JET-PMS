8043b9477cebd8489eb674ca8af641a39b31f8cf
hugohirling <hugo.hirling@web.de>
2023-06-21 18:19:10 +0200

    Removed old structure and added backend/frontend structure with react

--- src/main/java/com/hugohirling/objects/DRSA.java
+++
@@ -1,7 +0,0 @@
-package com.hugohirling.objects;
-
-public enum DRSA {
-    BRONZE,
-    SILBER,
-    GOLD
-}
--- src/main/java/com/hugohirling/objects/ImageRight.java
+++
@@ -1,7 +0,0 @@
-package com.hugohirling.objects;
-
-public enum ImageRight {
-    NO_IMAGES_ALLOWED,
-    ALL_IMAGES_ALLOWED,
-    ONLY_IMAGES_FOR_INTERN_USE_ALLOWED,
-}
