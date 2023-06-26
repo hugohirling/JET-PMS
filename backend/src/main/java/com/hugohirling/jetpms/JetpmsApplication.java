36471fb3cba596e5139cc279946e9b7a66e058bd
hugohirling <hugo.hirling@web.de>
2023-06-26 10:19:15 +0200

    Added Springboot-Framework

---
+++ backend/src/main/java/com/hugohirling/jetpms/JetpmsApplication.java
@@ -0,0 +1,13 @@
+package com.hugohirling.jetpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetpmsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hugohirling/jetpms/JetpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugohirling.jetpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetpmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
